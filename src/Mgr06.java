ad2c11d7165df734dbb5beb094a579f6c99d1c64
sicheng0302 <initial_lsc@126.com>
2021-07-04 22:54:32 +0800

    提交学习内容，加油，加油，加油

--- src/Mgr06.java
+++ src/Mgr06.java
@@ -1,6 +1,6 @@
 public class Mgr06 {
     /**
-     * 线程安全的单例
+     * 线程安全的单例，懒汉式
      */
     private static volatile Mgr06 INSTANCE;//在超高并发的情况下，可能读到版初始化的实例
 
@@ -22,6 +22,9 @@ public class Mgr06 {
         }
         return INSTANCE;
     }
+
+
+
 
     public static void main(String[] args) {
         for (int i=0;i<50;i++){
